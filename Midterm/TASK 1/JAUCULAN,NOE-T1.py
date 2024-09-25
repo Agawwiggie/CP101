8495ed16660344649b16d789b69c2e4539e856a4
Agawwiggie <noealexis547@gmail.com>
2024-09-25 23:08:05 +0800

    JAUCULAN,NOE-T1.py

---
+++ Midterm/TASK 1/JAUCULAN,NOE-T1.py
@@ -0,0 +1,8 @@
+string = int(input("Enter a number: "))
+
+if string > 0:
+  print("The number is positive.")
+elif string < 0:
+  print("The number is negative.")
+else:
+  print("The number is zero.")
